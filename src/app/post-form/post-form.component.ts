6a109a9d5b60d1b646bb5d80316f534712995e4d
Kunal Ojha <kunalojha@Kunals-MacBook-Pro.local>
2018-05-23 17:06:38 -0400

    editMode and create new post views configured

--- src/app/post-form/post-form.component.ts
+++ src/app/post-form/post-form.component.ts
@@ -8,6 +8,7 @@ import { PostsService } from '../posts/posts.service';
 import { Subscription, Observable } from 'rxjs';
 import { map, tap, withLatestFrom, startWith, share } from 'rxjs/operators';
 import { IBlogPost } from '../model/blogPost.model';
+import { deactivateEditMode } from '../store/actions/mode.actions';
 
 @Component({
   selector: 'app-post-form',
@@ -16,44 +17,39 @@ import { IBlogPost } from '../model/blogPost.model';
 })
 export class PostFormComponent implements OnInit, OnDestroy {
 
+  editMode$: Subscription;
   blog$: Subscription;
-  blogPost: IBlogPost = {
-    title : "Title of Blog Post",
-    description: "A brief description of the blog post entry",
-    imageUrl: "http://www-db.deis.unibo.it/courses/TW/DOCS/w3schools/w3css/img_mountains_wide.jpg"
-  };
-  id: number = 0;
-  editView: boolean;
-  preview: boolean = false;
+  blogPost: IBlogPost;
+  editMode: boolean;
 
 
-  constructor(private router: Router, private route: ActivatedRoute, private store: Store<IAppState>, private postsService: PostsService) {
-  }
+  constructor(private router: Router, private route: ActivatedRoute, private store: Store < IAppState > , private postsService: PostsService) {}
 
   ngOnInit() {
-    this.blog$ = this.store.select('posts', 'blogs').pipe(
-      withLatestFrom(this.route.params),
-      map(([blogs, params]) => this.blogPost = blogs[params.id]
-    )
-  ).subscribe()
-
-
-    // this.route.params.pipe(
-    //   tap(params => this.id = params.id),
-    //   withLatestFrom(this.blog$),
-    //   map(([params, blogs]) => this.blog$ = blogs[params.id]
-    //   )
-    // )
-    // .subscribe(x=> console.log(this.blog$))
+    this.editMode$ = this.store.select('mode', 'editMode').subscribe(editMode => this.editMode = editMode );
+    console.log(this.editMode);
+      if (!this.editMode) {
+        alert('case 1');
+          this.blogPost = {
+              title: "Title of Blog Post",
+              description: "A brief description of the blog post entry",
+              imageUrl: "http://www-db.deis.unibo.it/courses/TW/DOCS/w3schools/w3css/img_mountains_wide.jpg"
+          }
+      } else {
+        alert('case 2')
+          this.blog$ = this.store.select('posts', 'blogs').pipe(
+              withLatestFrom(this.route.params),
+              map(([blogs, params]) => this.blogPost = blogs[params.id])
+          ).subscribe()
+      }
   }
 
   onSubmitForm(entry) {
-    this.postsService.createPost(entry);
-    this.router.navigate(['/main']);
+      this.postsService.createPost(entry);
+      this.router.navigate(['/main']);
   }
 
   ngOnDestroy() {
-    this.editView = false;
+      this.editMode$.unsubscribe();
   }
-
 }
