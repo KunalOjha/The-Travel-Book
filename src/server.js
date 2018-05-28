12422e1cd134a952f4f9741f4dcb49b43076e01e
Kunal Ojha <kunalojha@Kunals-MacBook-Pro.local>
2018-05-28 13:49:40 -0500

    server.js change

--- src/server.js
+++ src/server.js
@@ -3,8 +3,9 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
-app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/dist/index.html');
+app.get('*', function (req, res) {
+  const index = path.join(__dirname, 'dist', 'index.html');
+  res.sendFile(index);
 });
 
 app.listen(process.env.PORT || 8080)
