17ab3c60150820783ee38c08ad288bc6a7f7027d
Kunal Ojha <kunalojha@Kunals-MacBook-Pro.local>
2018-05-28 02:08:31 -0400

    app is crashing after successful build. Attempt #1 to fix

--- src/server.js
+++ src/server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 8080);
-
-//PathLocationStategy, Angular handles the routing for the SPA, NOT the Node Server.
 app.all('*', (req, res) => {
   res.status(200).sendFile(__dirname + '/dist/index.html');
 });
+
+app.listen(process.env.PORT || 8080);
