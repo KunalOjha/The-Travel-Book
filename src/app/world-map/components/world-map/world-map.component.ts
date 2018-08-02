aabaf851895492451c3cc1a75abe01354ae6bab3
Kunal Ojha <kunalojha12@gmail.com>
2018-08-02 17:07:04 -0500

    leaflet map added to world-map component

--- src/app/world-map/components/world-map/world-map.component.ts
+++ src/app/world-map/components/world-map/world-map.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { tileLayer, latLng } from "leaflet";
 
 @Component({
-  selector: 'app-world-map',
-  templateUrl: './world-map.component.html',
-  styleUrls: ['./world-map.component.css']
+  selector: "app-world-map",
+  templateUrl: "./world-map.component.html",
+  styleUrls: ["./world-map.component.css"]
 })
 export class WorldMapComponent implements OnInit {
+  leafletZoom = 3;
+  options = {
+    layers: [
+      tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: "..."
+      })
+    ],
+    zoom: 3,
+    center: latLng(46.879966, -121.726909)
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/world-map/world-map.module.ts
+++ src/app/world-map/world-map.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { WorldMapRoutingModule } from "./world-map-routing.module";
 import { SharedModule } from "../shared/shared.module";
-import { WorldMapComponent } from './components/world-map/world-map.component';
+import { WorldMapComponent } from "./components/world-map/world-map.component";
+import { LeafletModule } from "@asymmetrik/ngx-leaflet";
 
 @NgModule({
-  imports: [SharedModule, WorldMapRoutingModule],
+  imports: [SharedModule, WorldMapRoutingModule, LeafletModule],
   declarations: [WorldMapComponent]
 })
 export class WorldMapModule {}
