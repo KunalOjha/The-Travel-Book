7b7c18db076afdc10373a8976d58ee28de5de454
Kunal Ojha <kunalojha12@gmail.com>
2018-08-22 16:55:07 -0500

    trying to load page after navigation end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,30 @@
-import { Component, OnInit } from "@angular/core";
-import { IAppState } from "./store/store";
-import { Store } from "../../node_modules/@ngrx/store";
-import { requestBlogPosts } from "./store/actions/blog.actions";
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { IAppState } from './store/store';
+import { Store } from '../../node_modules/@ngrx/store';
+import { requestBlogPosts } from './store/actions/blog.actions';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = "Travellist.io";
+  title = 'Travellist.io';
 
-  constructor(private store: Store<IAppState>) {}
+  constructor(
+    private store: Store<IAppState>,
+    private router: Router,
+    private cd: ChangeDetectorRef
+  ) {}
 
   ngOnInit() {
     this.store.dispatch(new requestBlogPosts());
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.cd.detectChanges();
+        alert('donezo');
+      }
+    });
   }
 }
